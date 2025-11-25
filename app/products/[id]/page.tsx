e3aff648f797caeb4ab2ef4f8a9721b7c80d36fd
yan fei <yan00126@algonquinlive.com>
2025-11-25 15:53:55 -0500

    set up review and rating component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,6 +6,8 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
 import ShareButton from "@/components/single-product/ShareButton";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import ProductReviews from "@/components/reviews/ProductReviews";
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
@@ -44,6 +46,10 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
           <AddToCart productId={params.id} />
         </div>
       </div>
+
+      {/* <ProductReviews productId={params.id} /> */}
+      <ProductReviews productId={params.id} />
+      <SubmitReview productId={params.id} />
     </section>
   );
 }
--- components/reviews/SubmitReview.tsx
+++ components/reviews/SubmitReview.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { SubmitButton } from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+import { Card } from "../ui/card";
+import RatingInput from "./RatingInput";
+import TextAreaInput from "../form/TextAreaInput";
+import { Button } from "../ui/button";
+import { createReviewAction } from "@/utils/actions";
+import { useUser } from "@clerk/nextjs";
 
-function SubmitReview() {
-  return <div>SubmitReview</div>;
+function SubmitReview({ productId }: { productId: string }) {
+  const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+  const { user } = useUser();
+  return (
+    <div>
+      <Button
+        size="lg"
+        className="capitalize"
+        onClick={() => setIsReviewFormVisible((prev) => !prev)}
+      >
+        leave review
+      </Button>
+      {isReviewFormVisible && (
+        <Card className="p-8 mt-8">
+          <FormContainer action={createReviewAction}>
+            <input type="hidden" name="productId" value={productId} />
+            <input
+              type="hidden"
+              name="authorName"
+              value={user?.firstName || "user"}
+            />
+            <input type="hidden" name="authorImageUrl" value={user?.imageUrl} />
+            <RatingInput name="rating" />
+            <TextAreaInput
+              name="comment"
+              labelText="feedback"
+              defaultValue="outstanding product!!!"
+            />
+            <SubmitButton className="mt-4" />
+          </FormContainer>
+        </Card>
+      )}
+    </div>
+  );
 }
 
 export default SubmitReview;
