42ba7e57bc0c9c68dd5b77fa5d9eab52391f2aa3
yan fei <yan00126@algonquinlive.com>
2025-11-27 10:42:26 -0500

    changing dummy review rating render to the real thing

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,11 +1,12 @@
+import { fetchProductRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
-function ProductRating({ productId }: { productId: string }) {
+async function ProductRating({ productId }: { productId: string }) {
   //temp
-  const rating = 4.5;
-  const count = 25;
+  const { rating, count } = await fetchProductRating(productId);
+
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
-  const countValue = `($count) reviews`;
+  const countValue = `(${count}) reviews`;
   return (
     <span className={className}>
       <FaStar className="w-3 h-3" />
--- utils/actions.ts
+++ utils/actions.ts
@@ -267,7 +267,23 @@ export const fetchProductReviews = async (productId: string) => {
   });
   return reviews;
 };
-export const fetchProductReviewsByUser = async () => {};
+export const fetchProductRating = async (productId: string) => {
+  const result = await db.review.groupBy({
+    by: ["productId"],
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: { productId },
+  });
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
+};
+
 export const deleteProductReviews = async () => {};
 export const findExistingReviews = async () => {};
-export const fetchProductRating = async () => {};
+export const fetchProductReviewsByUser = async () => {};
