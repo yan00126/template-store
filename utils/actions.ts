53c0805b4122f0bfb3598ec14eb8ec63cb849228
yan fei <yan00126@algonquinlive.com>
2025-11-29 14:34:06 -0500

    complete remove item in cart page

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 import db from "@/utils/db";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth, currentUser, getAuth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import {
   imageSchema,
@@ -482,9 +482,60 @@ export const addToCartAction = async (prevState: any, formData: FormData) => {
   redirect("/cart");
 };
 
-export const removeCartItemAction = async () => {};
+export const removeCartItemAction = async (
+  prevState: any,
+  FormData: FormData
+) => {
+  const user = await getAuthUser();
+  try {
+    const cartItemId = FormData.get("id") as string;
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.cartItem.delete({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+    });
+    await updateCart(cart);
+    revalidatePath("/cart");
+    return { message: "Item remove from cart" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 
-export const updateCartItemAction = async () => {};
+export const updateCartItemAction = async ({
+  amount,
+  cartItemId,
+}: {
+  amount: number;
+  cartItemId: string;
+}) => {
+  const user = await getAuthUser();
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.cartItem.update({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+      data: {
+        amount,
+      },
+    });
+    await updateCart(cart);
+    revalidatePath("/cart");
+    return { message: "cart  updated" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   return { message: "order created" };
