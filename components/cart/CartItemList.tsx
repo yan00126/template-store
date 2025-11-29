49430c762bd2bdce0368dbc9184c4dc1b796b122
yan fei <yan00126@algonquinlive.com>
2025-11-29 13:22:31 -0500

    shopping cart component added, page almost done, without amount and remove button

--- components/cart/CartItemList.tsx
+++ components/cart/CartItemList.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Card } from "../ui/card";
+import { FirstColumn, SecondColumn, FourthColumn } from "./CartItemColumns";
+import ThirdColumn from "./ThirdColumn";
+import { CartItemWithProduct } from "@/utils/types";
 
-function CartItemList() {
-  return <div>CartItemList</div>;
+function CartItemList({ cartItems }: { cartItems: CartItemWithProduct[] }) {
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { id, amount } = cartItem;
+        const { image, name, company, price, id: productId } = cartItem.product;
+        return (
+          <Card
+            key={id}
+            className="flex flex-col gap-y-4 md:flex-row flex-wrap p-6 mb-8 gap-x-4"
+          >
+            <FirstColumn image={image} name={name} />
+            <SecondColumn name={name} company={company} productId={productId} />
+            <ThirdColumn id={id} quantity={amount} />
+            <FourthColumn price={price} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CartItemList;
